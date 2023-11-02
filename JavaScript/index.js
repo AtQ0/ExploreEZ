979cc07db70bd7984b96150b1b5c1304d6f9d6df
AtQ0 <mail@atkobabic.com>
2023-11-02 10:43:02 +0100

    repairing layout

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,17 +1,3 @@
-/*====================================*/
-/*============= HAMBURGER ============*/
-/*====================================*/
-
-/*SELECT ELEMENTS FROM DOM*/
-const hamburgerContainer = document.getElementById("hamburger-container");
-const movingRectangleInHamburger = document.querySelector(".rectB");
-
-
-/*===== ANIMATION OF HAMBURGER =====*/
-
-/*ANIMATE HAMBURGER BY ADDING IT A NEW CSS-CLASS, ON HOVER OF PARENT*/
-hamburgerContainer.addEventListener("mouseover", function () {
-    movingRectangleInHamburger.classList.add("slide-rectB-to-the-right");
-});
-
-/*===== ON CLICK OF HAMBURGER =====*/
+/*===================================*/
+/*=== INTRO IMAGE HEIGHT ADJUSTER ===*/
+/*===================================*/
