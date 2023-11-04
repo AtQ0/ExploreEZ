3242881c8c8099b45b425f85d2c3b277baeb7473
AtQ0 <mail@atkobabic.com>
2023-11-04 10:38:07 +0100

    forms-menu-container
    just fixed so that the forms menu container is vissible and not vissible and that it toggles up and down on button click

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,32 @@
 /*===================================*/
 /*=== INTRO IMAGE HEIGHT ADJUSTER ===*/
 /*===================================*/
-const magicBtn = document.getElementById("");
+
+/*=====================================*/
+/*====== ANIMATION OF FORMS MENU ======*/
+/*=====================================*/
+
+//SELECT ELEMENTS FROM DOM
+const magicBtn = document.getElementById("magic-btn-wrapper");
+const formsMenu = document.getElementById("forms-menu-container");
+
+//Boolean to regulate if forms-menu is visible or not
+let isFormsMenuVisible = false;
+
+//Add new class (for animation) to forms-menu, on click-event
+magicBtn.addEventListener("click", function () {
+
+
+
+    if (isFormsMenuVisible === false) {
+        formsMenu.classList.remove("class-for-sliding-forms-menu-container-down");
+        formsMenu.classList.add("class-for-sliding-forms-menu-container-up");
+        isFormsMenuVisible = true;
+    }
+    else {
+        formsMenu.classList.remove("class-for-sliding-forms-menu-container-up");
+        formsMenu.classList.add("class-for-sliding-forms-menu-container-down");
+        isFormsMenuVisible = false;
+    }
+
+})
