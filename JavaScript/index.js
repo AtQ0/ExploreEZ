d1650b222671112a13936225cbf586af70d35f75
AtQ0 <mail@atkobabic.com>
2023-11-07 10:05:31 +0100

    other

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -16,8 +16,6 @@ let isFormsMenuVisible = false;
 //Add new class (for animation) to forms-menu, on click-event
 magicBtn.addEventListener("click", function () {
 
-
-
     if (isFormsMenuVisible === false) {
         formsMenu.classList.remove("class-for-sliding-forms-menu-container-down");
         formsMenu.classList.add("class-for-sliding-forms-menu-container-up");
