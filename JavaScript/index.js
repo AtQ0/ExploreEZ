c4a6e965b583a54331924736391d17c979fb0eff
AtQ0 <mail@atkobabic.com>
2023-11-08 10:29:57 +0100

    Populate Dropdown onstart
    Populated dropdown menu on start with js.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,4 @@
-/*===================================*/
-/*=== INTRO IMAGE HEIGHT ADJUSTER ===*/
-/*===================================*/
+
 
 /*=====================================*/
 /*====== ANIMATION OF FORMS MENU ======*/
@@ -28,3 +26,73 @@ magicBtn.addEventListener("click", function () {
     }
 
 })
+
+
+
+/*===================================*/
+/*=========== CALL ON API ===========*/
+/*===================================*/
+
+//SELECT ELEMENTS FROM THE DOM
+const dropDownMenuForSearching = document.getElementById("dropdown-menu-search");
+
+//CREAT VARIABLES TO BE USED
+let chosenCity;//chosen city should refer to dropdown
+let cititesIDArray = [];
+let cityNamesOnServerFromStartArray = [];
+let stringForPopulatingDropdownWithCityNames = "";
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("eventet funkar");
+
+    //Set chosen city to nothing so all cities are called
+    chosenCity = "";
+
+    //Call funtions that calls all citites
+    callCities(chosenCity);
+});
+
+
+let callCities = (cityInput) => {
+
+    console.log("funktionen körs");
+
+    fetch("https://avancera.app/cities/" + cityInput)
+        .then((response) => response.json())
+        .then((result) => {
+
+            console.log(result);
+
+            //Populate array with cities on web-server
+            for (let i = -1; i < result.length; i++) {
+
+                //First value of this array should always be All Cities
+                if (i === -1) {
+                    cityNamesOnServerFromStartArray[0] = "All cities";
+                }
+                //Else populate nextcoming indexes with real values from server
+                else {
+                    cityNamesOnServerFromStartArray[i + 1] = result[i].name;
+
+                }
+            }
+
+            //Create child (option) html-elements to parent (select) element
+            for (let j = 0; j < cityNamesOnServerFromStartArray.length; j++) {
+
+                //write all html-code elements in one long string
+                stringForPopulatingDropdownWithCityNames += `
+                <option value="`+ cityNamesOnServerFromStartArray[j] + `">` + cityNamesOnServerFromStartArray[j] + `</option>
+                `;
+            }
+
+            console.log(stringForPopulatingDropdownWithCityNames)
+
+            //Populate parent (select) html-element with option elements from string
+            dropDownMenuForSearching.innerHTML = stringForPopulatingDropdownWithCityNames;
+        });
+
+
+    console.log(cityNamesOnServerFromStartArray);
+}
