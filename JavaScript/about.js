8ed9c9e7d1edc1e80171973e72e84a799fa81ee2
AtQ0 <mail@atkobabic.com>
2023-11-10 12:11:15 +0100

    other
    all kind of things. buttons. icons svgs, background images

---
+++ JavaScript/about.js
@@ -0,0 +1,14 @@
+/*====================================*/
+/*======= HOME BTN CLICK EVENT =======*/
+/*====================================*/
+
+//SELECT ELEMENTS FROM DOM
+let homeBtn = document.getElementById("magic-button");
+
+
+//Add click event to homeBtn
+homeBtn.addEventListener("click", () => {
+
+    window.location.replace("../index.html")
+
+})
