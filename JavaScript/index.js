8f4aec097b4531a0d0ab9333b3176c22f05ba699
AtQ0 <mail@atkobabic.com>
2023-11-14 00:03:29 +0100

    Added and finished wikimedia
    Got lat and lon from wikipedia instead and could therefore delete mapquest query/function, and also solve the query done in the for loop (all cities), as well as single city. This is wrapped up now!

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -86,7 +86,6 @@ let callCitiesOnPageLoad = () => {
             }
 
             dropDownMenuForSearching.innerHTML = stringForPopulatingDropdownWithCityNames;
-            // console.log(stringForPopulatingDropdownWithCityNames);
 
         });
 
@@ -144,33 +143,52 @@ let writeCitiesAndElements = (chosenCityIdInput) => {
                 for (let i = 0; i < result.length; i++) {
 
                     //Call method for fetching geo-coordinates from MapQuest API
-                    const promiseForGeo = getGeoCoordinatesFromCity(result[i].name)
-                        .then((coordinates) => {
+                    const promiseForCitySummary = getCitySummary(result[i].name)
+                        .then((citySummaryObject) => {
                             //Store lat and lang in temp variables
-                            const lat = coordinates.lat;
-                            const lng = coordinates.lng;
+                            const cityPage = citySummaryObject.page;
+                            const lat = citySummaryObject.lat;
+                            const lon = citySummaryObject.lon;
+                            const cityExtract = citySummaryObject.description;
+
+                            //NOT USED YET
+                            const thumbnail = citySummaryObject.thumbnail;
+                            const originalImage = citySummaryObject.originalImage;
 
                             //Append data in string used for creating dynamic divs
                             stringForCreatingDynamicDivsInApiContentDiv += `
-                                <div class="dynamic-div-container">
-                                    <div class="dynamic-div-wrapper">
-                                        <h2>${result[i].name}</h2>
-                                        <p>Population: ${result[i].population}</p>
-                                        <div class="geo-coordinates-container">
-                                            <p>Latitude: ${lat}</p>
-                                            <p>Longitude: ${lng}</p>
+
+                                <div class="every-city-container">
+                                    <h2><a href="${cityPage}"  target="_blank">${result[i].name}</a></h2>
+                                    <p>Population: ${result[i].population}</p>
+                                    <div>
+                                        <p>Latitude: ${lat}</p>
+                                        <p>Longitude: ${lon}</p>
+                                        <div id="learn-more-container">
+                                            <div id="learn-more-wrapper">
+                                                <p>Learn more</p>
+                                                <div id="expand-btn-container">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#3A3320" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <p class="city-description">${cityExtract}</p>
+                                            </div>
                                         </div>
-                                        <div class="dynamic-btn-container">
-                                                <div id="change-btn"></div>
-                                                <div id="delete-btn"></div>
+                                        <div class="change-delete-btn-container">
+                                            <button id="change-btn">Change</button>
+                                            <button id="delete-btn">Delete</button>
                                         </div>
                                     </div>
                                 </div>
+
+
                             `;
                         });
 
                     //Store every promise inside of all promises arrayp
-                    promises.push(promiseForGeo);
+                    promises.push(promiseForCitySummary);
                 }
 
                 // Wait for all promises to resolve before updating the DOM
@@ -185,10 +203,12 @@ let writeCitiesAndElements = (chosenCityIdInput) => {
                 const city = result; // Single city object
 
                 //Call method for fetching city description from WikiMedia API
-                const promiseForCityDescription = getCityDescription(city.name)
+                getCitySummary(city.name)
                     .then((citySummaryObject) => {
-                        const cityDescription = citySummaryObject.description;
                         const cityPage = citySummaryObject.page;
+                        const lat = citySummaryObject.lat;
+                        const lon = citySummaryObject.lon;
+                        const cityExtract = citySummaryObject.description;
 
                         //NOT USED YET
                         const thumbnail = citySummaryObject.thumbnail;
@@ -202,43 +222,38 @@ let writeCitiesAndElements = (chosenCityIdInput) => {
                         // Clear the existing content by setting stringForCreatingDynamicDivsInApiContentDiv to an empty string
                         stringForCreatingDynamicDivsInApiContentDiv = '';
 
-                        // Fetch geo-coordinates for the selected city
-                        getGeoCoordinatesFromCity(city.name)
-                            .then((coordinates) => {
-                                const lat = coordinates.lat;
-                                const lng = coordinates.lng;
 
-                                //Put data in string used for creating a dynamic div
-                                stringForCreatingDynamicDivsInApiContentDiv = `
-                                    <div class="every-city-container">
-                                        <h2><a href="${cityPage}"  target="_blank">${city.name}</a></h2>
-                                        <p>Population: ${city.population}</p>
-                                        <div>
-                                            <p>Latitude: ${lat}</p>
-                                            <p>Longitude: ${lng}</p>
-                                            <div id="learn-more-container">
-                                                <div id="learn-more-wrapper">
-                                                    <p>Learn more</p>
-                                                    <div id="expand-btn-container">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#3A3320" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/>
-                                                        </svg>
-                                                    </div>
-                                                </div>
-                                                <div>
-                                                    <p class="city-description">${cityDescription}</p>
-                                                </div>
-                                            </div>
-                                            <div class="change-delete-btn-container">
-                                                <button id="change-btn">Change</button>
-                                                <button id="delete-btn">Delete</button>
+                        //Put data in string used for creating a dynamic div
+                        stringForCreatingDynamicDivsInApiContentDiv = `
+                            <div class="every-city-container">
+                                <h2><a href="${cityPage}"  target="_blank">${city.name}</a></h2>
+                                <p>Population: ${city.population}</p>
+                                <div>
+                                    <p>Latitude: ${lat}</p>
+                                    <p>Longitude: ${lon}</p>
+                                    <div id="learn-more-container">
+                                        <div id="learn-more-wrapper">
+                                            <p>Learn more</p>
+                                            <div id="expand-btn-container">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#3A3320" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/>
+                                                </svg>
                                             </div>
                                         </div>
+                                        <div>
+                                            <p class="city-description">${cityExtract}</p>
+                                        </div>
                                     </div>
-                                `;
+                                    <div class="change-delete-btn-container">
+                                        <button id="change-btn">Change</button>
+                                        <button id="delete-btn">Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
 
-                                //Add a dynamic div based on data in string variable
-                                apiContentDiv.innerHTML = stringForCreatingDynamicDivsInApiContentDiv;
-                            });
+                        //Add a dynamic div based on data in string variable
+                        apiContentDiv.innerHTML = stringForCreatingDynamicDivsInApiContentDiv;
+
 
                     })
 
@@ -251,53 +266,26 @@ let writeCitiesAndElements = (chosenCityIdInput) => {
 }
 
 
-//Fetch data from MapQuest API and return a promise
-function getGeoCoordinatesFromCity(selectedCityNameInput) {
-    return fetch('https://www.mapquestapi.com/geocoding/v1/address?key=eKH0ZMAEGWKaibhVOrRQftMpmBMpFcLT&location=' + selectedCityNameInput)
-        .then((response) => response.json())
-        .then((resultMapQuest) => {
-
-            //If request is successful (statuscode === 0), store lat and lng and return an object
-            if (resultMapQuest.info.statuscode === 0) {
-                const lat = resultMapQuest.results[0].locations[0].latLng.lat;
-                const lng = resultMapQuest.results[0].locations[0].latLng.lng;
-
-                // Return an object with lat and lng
-                return { lat, lng };
-            } else {
-                console.log("API request failed with status code: " + resultMapQuest.info.statuscode);
-                //Return an object with lat and lng as undefined
-                return { lat: undefined, lng: undefined };
-            }
-        })
-        .catch((error) => {
-            console.error("Error occurred while fetching data:", error);
-            //Return an object with lat and lng as undefined
-            return { lat: undefined, lng: undefined };
-        });
-}
-
-
 //Fetch data from WikiMedia API and return a promise
-function getCityDescription(cityNameInput) {
+function getCitySummary(cityNameInput) {
 
     //Set fetch adress (en or sv) depending on language you want to generate
-    return fetch('https://en.wikipedia.org/api/rest_v1/page/summary/' + cityNameInput)
+    return fetch('https://sv.wikipedia.org/api/rest_v1/page/summary/' + cityNameInput)
         .then((responseFromWikiMedia) => responseFromWikiMedia.json())
         .then((resultWikiMedia) => {
 
             //Find documentation to set up if status code successful or not
-            console.log(resultWikiMedia);
-            console.log(resultWikiMedia.extract + "woowowowo");
 
             //Store objects keys/property-values in variables
+            const lat = resultWikiMedia.coordinates.lat;
+            const lon = resultWikiMedia.coordinates.lon;
             const description = resultWikiMedia.extract;
             const page = resultWikiMedia.content_urls.desktop.page;
             const thumbnail = resultWikiMedia.thumbnail.source;
             const originalImage = resultWikiMedia.originalimage.source;
 
             //Return variables from fetch
-            return { description, page, thumbnail, originalImage };
+            return { page, lat, lon, description, thumbnail, originalImage };
         });
 
 
