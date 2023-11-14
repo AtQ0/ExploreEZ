c560e8c96c67c4a72baeed320c5cab21f6358e5c
AtQ0 <mail@atkobabic.com>
2023-11-14 12:12:51 +0100

    all working and good

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -168,7 +168,7 @@ let writeCitiesAndElements = (chosenCityIdInput) => {
                                             <div id="learn-more-wrapper">
                                                 <p>Learn more</p>
                                                 <div id="expand-btn-container">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#3A3320" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#3A3320" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/>
                                                     </svg>
                                                 </div>
                                             </div>
@@ -235,8 +235,8 @@ let writeCitiesAndElements = (chosenCityIdInput) => {
                                         <div id="learn-more-wrapper">
                                             <p>Learn more</p>
                                             <div id="expand-btn-container">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#3A3320" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18l6-6-6-6"/>
-                                                </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#3A3320" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M6 9l6 6 6-6"/>
+                                            </svg>
                                             </div>
                                         </div>
                                         <div>
