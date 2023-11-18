42b912ddbf1aa223e11ac2f39ac2c12c7f0345c7
AtQ0 <mail@atkobabic.com>
2023-11-18 18:32:16 +0100

    other

--- JavaScript/about.js
+++ JavaScript/about.js
@@ -10,3 +10,14 @@ let homeBtn = document.getElementById("magic-btn");
 homeBtn.addEventListener("click", () => {
     window.location.replace("index.html");
 })
+
+
+/*=====================================*/
+/*======== WEB STORAGE BUTTONS ========*/
+/*=====================================*/
+
+// Select elements from the DOM
+const firstThemeBtn = document.querySelector(".first-theme-container");
+const secondThemeBtn = document.querySelector(".second-theme-container");
+const backGround = document.querySelector(".top-or-left-intro-container");
+const foreGround = document.querySelector(".top-or-left-intro-wrapper");
